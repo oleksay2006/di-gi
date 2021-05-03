9c3140598cbbbd7b6c0c020f29c3c94e705e3e00
oleksay2006 <oleksay2006@gmail.com>
2021-05-03 19:22:48 +0300

    п
    р

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(document).ready(function() {
+  	$(".owl-carousel").owlCarousel({
+  		loop: true,
+  		items: 1,
+  		singleItem: true,
+    	slideSpeed : 1000,
+    	paginationSpeed : 1400,
+  	});
 });
